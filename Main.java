3c80ef941c09fe1fdbe2dac117c06d47a588e85b
Asadulnyc <asadulk@nycstudents.net>
2025-02-03 16:47:55 +0000

    midepage2

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import javax.swing.Box;
+
 public class Main {
     public static void main(String[] args) {
         Candy[][] box = new Candy[4][3];
@@ -7,5 +9,8 @@ public class Main {
         box[3][1] = new Candy("lemon");
         box[3][2] = new Candy("grape");
         System.out.println(box);
+
+        BoxOfCandy b = new BoxOfCandy(box);
+        System.out.println(b);
     }
 }
