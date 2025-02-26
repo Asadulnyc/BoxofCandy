e9688e4c327592d6ce05dce666316501f700e5c3
Asadulnyc <asadulk@nycstudents.net>
2025-02-26 16:49:09 +0000

    last thing in doc

--- Candy.java
+++ Candy.java
@@ -1,11 +1,13 @@
 public class Candy {
     private String flavor;
-    
+
     public Candy(String f) {
         flavor = f;
     }
+
     /** Returns a String representing the flavor of this piece of candy */
     public String getFlavor() {
+        return null;
         /* implementation not shown */ }
 
     // There may be instance variables, constructors, and methods that are not
